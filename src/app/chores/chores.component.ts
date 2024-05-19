ab4e09d0aba08216843cd7d0a16bfe99456a921f
wuyeelok <wuyeelok@gmail.com>
2024-05-19 22:05:22 +0800

    Setup: Not in order chores

--- src/app/chores/chores.component.ts
+++ src/app/chores/chores.component.ts
@@ -10,5 +10,36 @@ import { Component } from '@angular/core';
 export class ChoresComponent {
   doChores() {
     console.log('Begin do chores...');
+
+    this.closeWindow();
+    this.turnOnAC();
+    this.turnOnHeater();
+    this.turnOnWashingMachine();
+
+    console.log('Finish all chores!!!');
+  }
+
+  closeWindow() {
+    setTimeout(() => {
+      console.log('Window is closed!');
+    }, 3000);
+  }
+
+  turnOnAC() {
+    setTimeout(() => {
+      console.log('AC is ON!');
+    }, 1000);
+  }
+
+  turnOnHeater() {
+    setTimeout(() => {
+      console.log('heater is turn on!');
+    }, 1500);
+  }
+
+  turnOnWashingMachine() {
+    setTimeout(() => {
+      console.log('Washing Machine is on!');
+    }, 2500);
   }
 }
