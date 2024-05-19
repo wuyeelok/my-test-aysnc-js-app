47f53514a81be5ec1dfb8188e485a6920e398a44
wuyeelok <wuyeelok@gmail.com>
2024-05-19 22:23:33 +0800

    Refactor: Use Promise instead of callback hell

--- src/app/chores/chores.component.ts
+++ src/app/chores/chores.component.ts
@@ -11,6 +11,7 @@ export class ChoresComponent {
   doChores() {
     console.log('Begin do chores...');
 
+    /*
     // Callback hell
     this.closeWindow(() => {
       this.turnOnAC(() => {
@@ -19,33 +20,56 @@ export class ChoresComponent {
         });
       });
     });
+    */
+
+    this.closeWindow()
+      .then((msg) => {
+        console.log(msg);
+        return this.turnOnAC();
+      })
+      .then((msg) => {
+        console.log(msg);
+        return this.turnOnHeater();
+      })
+      .then((msg) => {
+        console.log(msg);
+        return this.turnOnWashingMachine();
+      })
+      .then((msg) => {
+        console.log(msg);
+        console.log('Finish all chores!!!');
+      });
   }
 
-  closeWindow(callBack: CallableFunction) {
-    setTimeout(() => {
-      console.log('Window is closed!');
-      callBack();
-    }, 3000);
+  closeWindow(): Promise<String> {
+    return new Promise((callInThen, callInCatch) => {
+      setTimeout(() => {
+        callInThen('Window is closed!');
+      }, 3000);
+    });
   }
 
-  turnOnAC(callBack: CallableFunction) {
-    setTimeout(() => {
-      console.log('AC is ON!');
-      callBack();
-    }, 1000);
+  turnOnAC(): Promise<String> {
+    return new Promise((callInThen, callInCatch) => {
+      setTimeout(() => {
+        callInThen('AC is ON!');
+      }, 1000);
+    });
   }
 
-  turnOnHeater(callBack: CallableFunction) {
-    setTimeout(() => {
-      console.log('heater is turn on!');
-      callBack();
-    }, 1500);
+  turnOnHeater(): Promise<String> {
+    return new Promise((callInThen, callInCatch) => {
+      setTimeout(() => {
+        callInThen('heater is turn on!');
+      }, 1500);
+    });
   }
 
-  turnOnWashingMachine(callBack: CallableFunction) {
-    setTimeout(() => {
-      console.log('Washing Machine is on!');
-      callBack();
-    }, 2500);
+  turnOnWashingMachine(): Promise<String> {
+    return new Promise((callInThen, callInCatch) => {
+      setTimeout(() => {
+        callInThen('Washing Machine is on!');
+      }, 2500);
+    });
   }
 }
